31628a2f4a07a9109bcbb34227d853c917026541
Clement-Plancon <plancon.clem@gmail.com>
2023-10-17 23:40:12 +0200

    architecture modification

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,9 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-  test: /\.svg$/,
-  use: ['file-loader']
-},
+        test: /\.svg$/,
+        use: ['file-loader']
+      },
     ],
   },
   resolve: {
@@ -27,7 +27,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
+    libraryTarget: 'umd', // Ceci est important
+    globalObject: 'this' // Ceci permet à la bibliothèque d'être utilisée à la fois avec des modules et dans un navigateur
   },
   plugins: [new MiniCssExtractPlugin()],
+  externals: { // Ceci est pour exclure React et ReactDOM de votre bundle
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+      umd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    },
+  },
   mode: 'production',
 };
