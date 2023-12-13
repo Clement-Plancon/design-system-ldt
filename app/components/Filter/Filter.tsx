c613b958dd6ce05b820cb11ebac6c63e6f3e61e0
Clement-Plancon <plancon.clem@gmail.com>
2023-12-13 14:13:12 +0100

    debug components

--- app/components/Filter/Filter.tsx
+++ app/components/Filter/Filter.tsx
@@ -1,63 +1,71 @@
-import React, { useState, useRef, useEffect, ReactNode } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './filter.module.scss';
-const chevronClose = '/src/images/input-dropdown/chevron-close.svg';
-const chevronOpen = '/src/images/input-dropdown/chevron-open.svg';
 
 export interface FilterProps {
   label: string;
-  options: Array<string | ReactNode>;
-  onSelect?: (selectedOption: string | ReactNode) => void;
+  options: Array<any>;
+  onSelect?: (selectedOption: string) => void;
   className?: string;
+  iconOpen?: React.ReactNode;
+  iconClose?: React.ReactNode;
 }
 
-const Filter: React.FC<FilterProps> = ({ label, options, onSelect, className }) => {
+const Filter: React.FC<FilterProps> = ({ label, options, onSelect, className, iconOpen, iconClose }) => {
   const [showDropdown, setShowDropdown] = useState(false);
-  const [selectedLabel, setSelectedLabel] = useState<ReactNode | string>(label);
-  const [isFilled, setIsFilled] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState<Set<string>>(new Set());
   const dropdownRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
+    };
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setShowDropdown(false);
-    }
-  };
-
-  const handleOptionClick = (option: ReactNode | string) => {
-    if (typeof option === 'string') {
-      setSelectedLabel(option);
+  const handleOptionClick = (option: string) => {
+    const newSelectedOptions = new Set(selectedOptions);
+    if (newSelectedOptions.has(option)) {
+      newSelectedOptions.delete(option);
     } else {
-      setSelectedLabel('Selected Option'); // ou autre placeholder si l'option est un composant
+      newSelectedOptions.add(option);
     }
+    setSelectedOptions(newSelectedOptions);
 
     if (onSelect) {
       onSelect(option);
-      setIsFilled(false);
     }
-    setIsFilled(true);
- 
+  };
+
+  const renderLabel = () => {
+    const count = selectedOptions.size;
+    return count === 0 ? label : `${count} filtre(s) sélectionné(s)`;
+  };
+
+  const renderIcon = () => {
+    const icon = showDropdown ? iconOpen : iconClose;
+    return <span className={styles['filter-icon']}>{icon}</span>;
   };
 
   return (
     <div className={`${styles.filter} ${className || ''}`} ref={dropdownRef}>
       <div
-        className={`${styles['filter__block']} ${styles['filter__block--default']} ${isFilled ? styles['filter__block--default--filled'] : ''}`}
+        className={`${styles['filter__block']} ${styles['filter__block--default']} ${selectedOptions.size > 0 ? styles['filter__block--default--filled'] : ''}`}
         onClick={() => setShowDropdown(!showDropdown)}
       >
-        {selectedLabel}
-        <img className={styles['filter-icon']} src={showDropdown ? chevronOpen : chevronClose} alt="chevron" />
+        {renderLabel()}
+        {renderIcon()}
       </div>
       {showDropdown && (
         <div className={styles['dropdown']}>
           {options.map((option, index) => (
             <div
               key={index}
+              className={selectedOptions.has(option) ? styles.selected : ''}
               onClick={() => handleOptionClick(option)}
             >
               {option}
